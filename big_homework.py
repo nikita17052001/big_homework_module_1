ba4a856caeae2ef82bb1fd06924eff115c5fdab8
nikita17052001 <nikita.loskutov2001@gmail.com>
2024-10-01 22:08:10 +0300

    Add files via upload

---
+++ big_homework.py
@@ -0,0 +1,11 @@
+grades = [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5]]
+students = sorted({'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'})
+grades = (sum(grades[0])/len(grades[0])), (sum(grades[1])/len(grades[1])), (sum(grades[2])/len(grades[2])), (sum(grades[3])/len(grades[3])), (sum(grades[4])/len(grades[4]))
+class_journal = dict(zip(students, grades))
+print(class_journal)
+
+
+
+
+
+
